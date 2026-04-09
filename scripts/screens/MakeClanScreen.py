92d988173e787d20605853e17d15306dc9c1ea0b
noobidoo <ttraskback@gmail.com>
2026-04-10 01:05:21 +0300

    Rename medicine_cat to healer in MakeClanScreen class for clarity

--- scripts/screens/MakeClanScreen.py
+++ scripts/screens/MakeClanScreen.py
@@ -4531,7 +4531,7 @@ class MakeClanScreen(Screens):
             game.clan = Clan(name = self.clan_name,
                             leader = self.leader,
                             deputy = self.deputy,
-                            medicine_cat = self.med_cat,
+                            healer = self.med_cat,
                             biome = self.biome_selected,
                             camp_bg = convert_camp[self.selected_camp_tab],
                             symbol=self.symbol_selected,
